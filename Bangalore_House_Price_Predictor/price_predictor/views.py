6d611d7f534f923fd7ec2e791dd69658ebee85c9
Utsab Dahal <70103872+meutsabdahal@users.noreply.github.com>
2024-06-19 17:21:35 +0545

    location value added from csv file

--- Bangalore_House_Price_Predictor/price_predictor/views.py
+++ Bangalore_House_Price_Predictor/price_predictor/views.py
@@ -1,6 +1,14 @@
+import pandas as pd
 from django.shortcuts import render
+
+data = pd.read_csv('../Clean_Bengaluru_House_Data.csv')
+
+locations = sorted(data['location'].unique())
+context = {
+    'locations': locations,
+}
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'price_predictor/home.html')
+    return render(request, 'price_predictor/home.html', context)
